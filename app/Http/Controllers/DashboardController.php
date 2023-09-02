1c8276a7aba5f61c0320d86e4c24944b7c49036b
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2023-09-01 21:32:49 -0400

    created controllers for welcome and dashboard vues

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request) {
+        return Inertia::render('Dashboard');
+    }
+}
